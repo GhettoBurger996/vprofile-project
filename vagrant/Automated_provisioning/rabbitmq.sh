eb5327fcb5093cbe361e8bab8013843957323260
ghettoburger996 <yusufnyasin@gmail.com>
2022-07-20 23:13:24 +0200

    (ref): rabbitmq shell script

--- vagrant/Automated_provisioning/rabbitmq.sh
+++ vagrant/Automated_provisioning/rabbitmq.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
 # system updates and installs
-sudo yum install epel-release -y
 sudo yum update -y
+sudo yum install epel-release -y
 sudo yum install wget -y
 sudo yum install socat -y
 cd /tmp/
 
 # erlang
-wget https://packages.erlang-solutions.com/erlang/rpm/centos/7/x86_64/esl-erlang_23.3.1-1~centos~7_amd64.rpm
+sudo wget https://packages.erlang-solutions.com/erlang/rpm/centos/7/x86_64/esl-erlang_23.3.1-1~centos~7_amd64.rpm
 sudo yum install esl-erlang*.rpm -y
 
 
 # rabbitmq
-wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.19/rabbitmq-server-3.8.19-1.el7.noarch.rpm
+sudo wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.19/rabbitmq-server-3.8.19-1.el7.noarch.rpm
+# sudo rpm --import https://www.rabbitmq.com/rabbitmq-release-signing-key.asc
 sudo yum install rabbitmq-server*.rpm -y
 sudo systemctl start rabbitmq-server
 sudo systemctl enable rabbitmq-server
 sudo systemctl status rabbitmq-server
+
 sudo sh -c 'echo "[{rabbit, [{loopback_users, []}]}]." > /etc/rabbitmq/rabbitmq.config'
 sudo rabbitmqctl add_user test test
 sudo rabbitmqctl set_user_tags test administrator
